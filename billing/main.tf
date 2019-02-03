48015a86eda461d99b580b69db5922acbe5bd28e
Richard Hughes <eeuc40@hotmail.com>
2019-02-03 19:53:15 +0000

    Add billing budget

---
+++ billing/main.tf
@@ -0,0 +1,9 @@
+resource "aws_budgets_budget" "monthly_budget" {
+  name = "Monthly Buget"
+  budget_type = "COST"
+  limit_amount = "${var.monthly_budget_amount}"
+  limit_unit = "USD"
+  time_period_end = "2087-06-15_00:00"
+  time_period_start = "2019-01-01_00:00"
+  time_unit = "MONTHLY"
+}
---
+++ billing/vars.tf
@@ -0,0 +1,3 @@
+variable "monthly_budget_amount" {
+  description = "The amount to budget per month in USD"
+}
