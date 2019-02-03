4698cfda06fc2950e1ad43c8c4b6ab5cdf3216e2
Richard Hughes <eeuc40@hotmail.com>
2019-02-03 20:04:11 +0000

    revert date change

--- billing/main.tf
+++ billing/main.tf
@@ -11,7 +11,7 @@ resource "aws_budgets_budget" "monthly_budget" {
   budget_type = "COST"
   limit_amount = "${var.monthly_budget_amount}"
   limit_unit = "USD"
-  time_period_end = "2099-06-15_00:00"
+  time_period_end = "2087-06-15_00:00"
   time_period_start = "2019-01-01_00:00"
   time_unit = "MONTHLY"
 }
