e54eb69fa5e0782664262197963c88ccaafa4d7c
Richie Hughes <eeuc40@hotmail.com>
2019-03-01 15:35:53 +0000

    Update vars.tf

--- route53/vars.tf
+++ route53/vars.tf
@@ -1,10 +1,3 @@
-variable "aws_region" {
-  description = "The region to deploy this resource in"
-
-  // Set since Route53 is global
-  default = "eu-west-1"
-}
-
 variable "domain_name" {
   description = "The domain you wish to create a hosted zone for"
 }
