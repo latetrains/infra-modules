dd6bdf3ce474c574e7e997a72bb5ee4bbcc54ace
Richard Hughes <richard.hughes@sykescottages.co.uk>
2019-03-22 15:02:30 +0000

    Set evaulate target health to false

--- route53/alias/main.tf
+++ route53/alias/main.tf
@@ -4,7 +4,8 @@ resource "aws_route53_record" "alias_dns_record" {
   type    = "A"
 
   alias {
-    name    = "${var.dns_name}"
-    zone_id = "${var.hosted_zone_id}"
+    name                   = "${var.dns_name}"
+    zone_id                = "${var.hosted_zone_id}"
+    evaluate_target_health = false
   }
 }
