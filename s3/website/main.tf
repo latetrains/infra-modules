1f7fcddaaeba8b82214433f6a90a63b57fa38f16
Richard Hughes <eeuc40@hotmail.com>
2019-03-24 17:59:40 +0000

    Fix principals

--- s3/website/main.tf
+++ s3/website/main.tf
@@ -18,7 +18,11 @@ data "aws_iam_policy_document" "public_read_document" {
   statement {
     actions   = ["s3:GetObject"]
     resources = ["${aws_s3_bucket.main_bucket.arn}/*"]
-    principals = ["*"]
+
+    principals {
+      type        = "*"
+      identifiers = ["*"]
+    }
   }
 }
 
