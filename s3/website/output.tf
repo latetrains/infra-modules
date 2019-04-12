ec2e1ca5eaea0482a6bc86233d23de2d3cd14d79
Richard Hughes <richard.hughes@sykescottages.co.uk>
2019-04-12 14:11:41 +0100

    Add outputs

--- s3/website/output.tf
+++ s3/website/output.tf
@@ -2,6 +2,10 @@ output "bucket_domain_name" {
   value = "${aws_s3_bucket.main_bucket.bucket_domain_name}"
 }
 
+output "bucket_regional_domain_name" {
+  value = "${aws_s3_bucket.main_bucket.bucket_regional_domain_name}"
+}
+
 output "bucket_arn" {
   value = "${aws_s3_bucket.main_bucket.arn}"
 }
