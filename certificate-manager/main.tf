bf773303e6b54ddff96d5bd58e16097e3dc2eed7
Richard Hughes <eeuc40@hotmail.com>
2019-04-25 20:52:33 +0100

    Add policy doc

--- certificate-manager/main.tf
+++ certificate-manager/main.tf
@@ -29,8 +29,3 @@ resource "aws_route53_record" "cert_validation" {
   records = ["${aws_acm_certificate.cert.domain_validation_options.0.resource_record_value}"]
   ttl     = 60
 }
-
-resource "aws_acm_certificate_validation" "cert" {
-  certificate_arn         = "${aws_acm_certificate.cert.arn}"
-  validation_record_fqdns = ["${aws_route53_record.cert_validation.fqdn}"]
-}
--- lambda/iam.tf
+++ lambda/iam.tf
@@ -12,7 +12,10 @@ data "aws_iam_policy_document" "lambda_role_policy" {
       ]
     }
   }
+}
 
+
+data "aws_iam_policy_document" "lambda_role_policy_document" {
   statement {
     actions   = "${var.role_actions}"
     resources = "${var.role_resources}"
@@ -22,4 +25,10 @@ data "aws_iam_policy_document" "lambda_role_policy" {
 resource "aws_iam_role" "lambda_iam_role" {
   name               = "${var.role_name}"
   assume_role_policy = "${data.aws_iam_policy_document.lambda_role_policy.json}"
+}
+
+
+resource "aws_iam_role_policy" "" {
+  policy = "${data.aws_iam_policy_document.lambda_role_policy_document.json}"
+  role = "${aws_iam_role.lambda_iam_role.id}"
 }
